54d63004617c1a253927689676b1c7ea6c5b7dde
Marmblshko <mks316skat@gmail.com>
2023-11-09 20:30:16 +0600

    small refactor and added comments destroy options. In posts temporarily took away subj and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,28 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: %i[create]
+  before_action :authenticate_user!, only: %i[create destroy]
+  before_action :set_post, only: %i[create destroy]
+
   def create
-    @post = Post.find(params[:post_id])
     @post.comments.create(comment_params) do |c|
       c.author = current_user.username
     end
 
-    redirect_to posts_path(@post)
+    redirect_to @post
+  end
+
+  def destroy
+    @comment = @post.comments.find params[:id]
+    @comment.destroy
+    redirect_to @post
   end
 
   private
 
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
   def comment_params
-    params.require(:comment).permit(:author,:body)
+    params.require(:comment).permit(:author, :body)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
-  has_many :comments
-
-  def subject
-    title
-  end
-
-  def last_comment
-    comments.last
-  end
+  has_many :comments, dependent: :destroy
 end
